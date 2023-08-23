354dd8bc5e253110a7a7f1168ad07cd347e07468
ateshun <emmanuel.ackah@azubiafrica.org>
2023-08-23 18:01:33 +0000

    first react file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world.</h1>
+      <p>I am Emmanuel Ackah Tanoe Eshun, this is my first React</p>
+    </div>
+  );
+}
+
+export default App;
